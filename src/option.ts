272180e8a503d1dfe953c97935f6694913a8ca25
Lucas Vieira <vieiralucas4@gmail.com>
2017-09-09 14:42:48 -0300

    Implementation of Option

---
+++ src/option.ts
@@ -0,0 +1,50 @@
+export abstract class Option<T> {
+  abstract isDefined(): boolean
+
+  isEmpty(): boolean {
+    return !this.isDefined()
+  }
+
+  abstract orElse(value: T): T
+
+  abstract map<T2>(fn: (value: T) => T2): Option<T2>
+}
+
+export class Some<T> extends Option<T> {
+  private value: T
+
+  constructor(value: T) {
+    super()
+    this.value = value
+  }
+
+  isDefined() {
+    return true
+  }
+
+  orElse(el: T): T {
+    if (this.value === null) {
+      return el
+    }
+
+    return this.value
+  }
+
+  map<T2>(fn: (value: T) => T2): Option<T2> {
+    return new Some(fn(this.value))
+  }
+}
+
+export class None<T> extends Option<T> {
+  isDefined() {
+    return false
+  }
+
+  orElse(value: T) {
+    return value
+  }
+
+  map<T2>(fn: (value: T) => T2): Option<T2> {
+    return new None()
+  }
+}
---
+++ test/option.test.ts
@@ -0,0 +1,67 @@
+import { expect } from 'chai'
+import * as Option from '../src/option'
+
+describe('Option', () => {
+  describe('.isDefined', () => {
+    it('should be true for Some', () => {
+      const n = new Option.Some(1)
+      expect(n.isDefined()).to.be.true
+    })
+
+    it('should be false for None', () => {
+      const n = new Option.None()
+      expect(n.isDefined()).to.be.false
+    })
+  })
+
+  describe('.isEmpty', () => {
+    it('should be false for Some', () => {
+      const n = new Option.Some(1)
+      expect(n.isEmpty()).to.be.false
+    })
+
+    it('should be true for None', () => {
+      const n = new Option.None()
+      expect(n.isEmpty()).to.be.true
+    })
+  })
+
+  describe('.orElse', () => {
+    it('should return value if it is a Some', () => {
+      const s = new Option.Some(2)
+      const two = s.orElse(3)
+
+      expect(two).to.equal(2)
+    })
+
+    it('should return else if it is a None', () => {
+      const n = new Option.None()
+      const two = n.orElse(2)
+
+      expect(two).to.equal(2)
+    })
+  })
+
+  describe('.map', () => {
+    it('should remain None', () => {
+      const n = new Option.None()
+      const n2 = n.map(x => 2)
+
+      expect(n2.isEmpty()).to.be.true
+    })
+
+    it('should map a Some', () => {
+      const two = new Option.Some(2)
+      const three = two.map(n => 3)
+
+      expect(three.orElse(1)).to.equal(3)
+    })
+
+    it('should enable mapping to another type', () => {
+      const n = new Option.Some(1)
+      const s = n.map(n => String(n))
+
+      expect(s.orElse('')).to.equal('1')
+    })
+  })
+})
